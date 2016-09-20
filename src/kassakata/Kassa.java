91bc71274918938a67182e3da0a64c5cc904a29f
Dojo <dojo@MacBook-Pro-van-Johan-Eikelboom.local>
2016-09-20 17:42:11 +0200

    layout

--- src/kassakata/Kassa.java
+++ src/kassakata/Kassa.java
@@ -13,15 +13,15 @@ package kassakata;
  * Als het totaalbedrag betaald is gaat de bon naar het voorraad systeem en het financiele systeem.
  * 
  * Prijzen (voorbeeld)
- * Handzeep Nivea 1,25
- * Handzeep Dove 1,55
- * Scheerzeep Nivea 2,50
- * Aardbeien Jam De Betuwe 1,35
- * Pruimen Jam De Betuwe 1,60
- * Stroop Huismerk 1,55
- * Brood volkoren 1.95
- * Korting
+ * Handzeep      Nivea      1,25
+ * Handzeep      Dove       1,55
+ * Scheerzeep    Nivea      2,50
+ * Aardbeien Jam De Betuwe  1,35
+ * Pruimen Jam   De Betuwe  1,60
+ * Stroop        Huismerk   1,55
+ * Brood volkoren           1.95
  * 
+ * Korting:
  * Handzeep Dove drie voor vier euro
  * Alle soorten Jam 3e gratis
  * Alle Nivea producten Tweede gratis mix en match.
--- test/kassakata/KassaTest.java
+++ test/kassakata/KassaTest.java
@@ -8,4 +8,7 @@ public class KassaTest {
 	public void test() {
 		
 	}
+	
+	
+	
 }
